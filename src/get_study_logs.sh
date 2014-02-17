a6b403d567fe170fb3034ed10b93eda5ba034569
Andy Wolff <andywolff@wpi.edu>
2014-02-17 16:36:17 -0500

    added query in C and added TreeParams msg

--- src/get_study_logs.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-rm /tmp/states.csv
-rm /tmp/actions.csv
-php get_study_logs.php
-cp /tmp/states.csv states.csv
-cp /tmp/actions.csv actions.csv
-
--- src/whackamole_learning_node.cpp
+++ src/whackamole_learning_node.cpp
@@ -4,13 +4,28 @@
 #include "std_msgs/Int32MultiArray.h"
 #include "std_msgs/Empty.h"
 
+#include "whackamole_learning/TreeParams.h"
+
+#include <mysql/mysql.h>
+
 #include <exception>
 #include <iostream>
 #include <vector>
+#include <string>
+#include <sstream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fstream>
+
 #include <GClasses/GApp.h>
 #include <GClasses/GError.h>
 #include <GClasses/GRand.h>
 #include <GClasses/GDecisionTree.h>
+
+// Database credentials
+#define SERVER "localhost"
+#define USER "root"
+#define DATABASE "rail_andy_db"
 
 /**** Node responsible for whackamole learning. */
 
@@ -37,28 +52,112 @@ static int moleStates[7];
 static int armPos;
 static int robotPos;
 
+// MySQL connection
+static MYSQL* conn;
+
 // Creates the decision tree used in determining autonomous whackamole behavior.
 void createDecisionTree(GDecisionTree &dtree)
 {
   //mole0,mole1,mole2,mole3,mole4,mole5,mole6,robotPos,armPos
-// Define the feature attributes (or columns)
-	vector<size_t> feature_values;
-	int i=0;
-	for (i=0; i<7; i++) feature_values.push_back(0); //7 continuous moles
-	feature_values.push_back(3); // armPos={0=left,1=mid,2=right}
-	feature_values.push_back(3); // robotPos={0=left,1=mid,2=right}
+  // Define the feature attributes (or columns)
+  vector<size_t> feature_values;
+  int i=0;
+  for (i=0; i<7; i++) feature_values.push_back(0); //7 continuous moles
+  feature_values.push_back(3); // armPos={0=left,1=mid,2=right}
+  feature_values.push_back(3); // robotPos={0=left,1=mid,2=right}
+  
+  // Define the label attributes (or columns)
+  vector<size_t> label_values;
+  label_values.push_back(6); // action: {0-2=armPos, 3-5=robotPos}
+  
+  // Load the state and action data into GMatrix objects
+  GMatrix features(feature_values); // states
+  //GMatrix features;
+  //features.loadArff("/home/andywolff/ros-groovy/src/whackamole_learning/whackamole.arff");
 
-	// Define the label attributes (or columns)
-	vector<size_t> label_values;
-	label_values.push_back(6); // action: {0-2=armPos, 3-5=robotPos}
+  GMatrix labels(label_values); // actions
 
-	// Load the state and action data into GMatrix objects
-	GMatrix features(feature_values); // states
-	GMatrix labels(label_values); // actions
+  // Get the feature data from the database
+  if (mysql_query(conn, "SELECT state_action_logs.mole0, state_action_logs.mole1, state_action_logs.mole2, state_action_logs.mole3, state_action_logs.mole4, state_action_logs.mole5, state_action_logs.mole6, state_action_logs.robotPos, state_action_logs.armPos FROM game_data INNER JOIN state_action_logs ON game_data.gameID = state_action_logs.gameID")) 
+  {
+    fprintf(stderr, "%s\n", mysql_error(conn));
+    mysql_close(conn);
+    exit(1);
+  }
+  
+  MYSQL_RES *feature_result = mysql_store_result(conn);
+  
+  if (feature_result == NULL) 
+  {
+    fprintf(stderr, "%s\n", mysql_error(conn));
+    mysql_close(conn);
+    exit(1);
+  }
+
+  int num_fields = mysql_num_fields(feature_result);
+
+  MYSQL_ROW row;
+  int rowNum = 0;
+  
+  while ((row = mysql_fetch_row(feature_result))) 
+  { 
+    features.newRow();
+    for(int i = 0; i < num_fields; i++) 
+    { 
+      printf("%s ", row[i] ? row[i] : "NULL");
+      features[rowNum][i] = atoi(row[i]); 
+    } 
+    printf("\n");
+
+    rowNum++; 
+  }
+  printf("\n");
+  
+  mysql_free_result(feature_result);	
+
+
+  // Get the label data from the database
+  if (mysql_query(conn, "SELECT state_action_logs.action FROM game_data INNER JOIN state_action_logs ON game_data.gameID = state_action_logs.gameID"))
+  {
+    fprintf(stderr, "%s\n", mysql_error(conn));
+    mysql_close(conn);
+    exit(1);
+  }
+
+  MYSQL_RES *label_result = mysql_store_result(conn);
+
+  if (label_result == NULL)
+  {
+    fprintf(stderr, "%s\n", mysql_error(conn));
+    mysql_close(conn);
+    exit(1);
+  }
+
+  num_fields = mysql_num_fields(label_result);
+
+  rowNum = 0;
+
+  while ((row = mysql_fetch_row(label_result)))
+  {
+    labels.newRow();
+    for(int i = 0; i < num_fields; i++)
+    {
+      printf("%s ", row[i] ? row[i] : "NULL");
+      labels[rowNum][i] = atoi(row[i]); 
+    }
+    printf("\n");
+
+    rowNum++;
+  }
+  printf("\n");
+
+  mysql_free_result(label_result);
+
+
+
+	//features.loadCsv("/home/andywolff/ros-groovy/src/whackamole_learning/src/states.csv",',');
 	
-	features.loadCsv("/home/andywolff/ros-groovy/src/whackamole_learning/src/states.csv",',');
-	
-	labels.loadCsv("/home/andywolff/ros-groovy/src/whackamole_learning/src/actions.csv",',');
+	//labels.loadCsv("/home/andywolff/ros-groovy/src/whackamole_learning/src/actions.csv",',');
 	
 	// Train the decision tree based on the data
 	dtree.train(features, labels);
@@ -74,6 +173,8 @@ void initializeTree (GDecisionTree &decisionTree)
 	int nRet = 0;
 	try
 	{
+		
+		treeInitialized = 0;
 		createDecisionTree(decisionTree);
 		treeInitialized = 1;
 
@@ -91,7 +192,10 @@ void initializeTree (GDecisionTree &decisionTree)
                 //std::vector<const char *> armPosVector {"left", "middle", "right"};
                 featureRelation.addAttribute("armPos", 3, NULL);
 
-                decisionTree.print(cout);//, &featureRelation, NULL);
+                std::ofstream treeFile;
+                treeFile.open ("/home/andywolff/ros-groovy/src/whackamole_learning/decisionTree.txt");
+                decisionTree.print(treeFile);//, &featureRelation, NULL);
+                treeFile.close();
 	}
 	catch(const std::exception& e)
 	{
@@ -176,6 +280,11 @@ void timeLeftCallback(const std_msgs::Int16::ConstPtr& msg)
   }
 }
 
+void createTreeCallback(const whackamole_learning::TreeParams::ConstPtr& msg)
+{
+  ROS_INFO("Create tree callback");
+}
+
 /**
  * Chooses an action based on the decision tree and the current mole states
  */
@@ -224,7 +333,9 @@ int main(int argc, char **argv)
   ros::Subscriber gameStartedSub = n.subscribe("whackamole/game_started", 1000, gameStartedCallback);
 
   ros::Subscriber timeLeftSub = n.subscribe("whackamole/time_left", 1000, timeLeftCallback);
-  
+ 
+  ros::Subscriber createTreeSub = n.subscribe("whackamole/create_tree", 1000, createTreeCallback);
+ 
   // Set initial flag values
   gameStarted = 0;
   autonomousMode = 0;
@@ -234,6 +345,19 @@ int main(int argc, char **argv)
 
   robotPos = 1;
   armPos = 1;  
+
+  /* Set up mySQL connection */
+  MYSQL_RES *res;
+  MYSQL_ROW row;
+  conn = mysql_init(NULL);
+  /* Connect to database */
+  char *pwd = getenv("WHACKAMOLE_DB_PW");
+  if (!mysql_real_connect(conn, SERVER,
+        USER, pwd, DATABASE, 0, NULL, 0)) {
+     fprintf(stderr, "%s\n", mysql_error(conn));
+     exit(1);
+  }
+  /*****End of MySQL connection setup*****/
 
   // On startup, attempt to create the decision tree.
   GDecisionTree decisionTree;
@@ -246,6 +370,11 @@ int main(int argc, char **argv)
  
     //ROS_INFO("%d %d %d %d %d", autonomousMode, newState, newActionAllowed, treeInitialized, gameStarted);
  
+    // If the decision tree has not been initialized, initialize the tree
+    if(!treeInitialized) {
+      initializeTree(decisionTree);
+    }
+
     // Only take actions if running in autonomous mode, the decision tree
     // has been initialized, a new mole state has been received since the 
     // last action, and a new action is allowed.
